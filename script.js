2c0b586a16f25757be1f90f4c94de9c9291604d0
Ashley <ashleyh5440@gmail.com>
2023-10-23 16:03:38 -0400

    edited comments

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {
-  // TODO: Add a listener for click events on the save button. 
   const currentHour = new Date().getHours(); //defines current hour
   console.log(currentHour);
   
@@ -15,7 +10,7 @@ $(function () {
     localStorage.setItem(time, task); //sets in local storage
   })
  
-//display the current date/time  
+//displays the current date/time  
 function updateCurrentTime() { //gets current time
   let currentTime = dayjs();
   let formattedTime = currentTime.format("h:mm A");
@@ -31,6 +26,8 @@ setInterval(updateCurrentTime, 60000); //update every 60 sec
 //comparing current time to time-block for past present future class
 $(".time-block").each(function () { //selects all elements with time-block class
   const blockHour = parseInt($(this).attr("id").split("-")[1]); //get hour from block's id
+  //split turns string into array, loops through the array 
+  //hour becomes index 0, the number becomes 1
 
   if (blockHour < currentHour) {
     $(this).addClass('past');
@@ -43,9 +40,9 @@ $(".time-block").each(function () { //selects all elements with time-block class
     $(this).addClass('future');
    }
   });
-  for (let i = 9; i < 18; i++){
-  $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i))}
 
-  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements.  
+  //retrieves items from local storage
+  for (let i = 9; i < 17; i++){ //uses for loop instead of writing out code for each timeblock
+  $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i))}
   
 });
